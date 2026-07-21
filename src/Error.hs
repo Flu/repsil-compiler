1960fe180004f1984de40cca086c1f3b7c05a9e9
Adrian Fluturel <fluturel.adrian@gmail.com>
2026-07-21 04:22:15 +0200

    Forgot to commit Error file...

---
+++ src/Error.hs
@@ -0,0 +1,16 @@
+module Error(ImportError(..), CompileError(..)) where
+
+import Data.Void (Void)
+import Data.Text as T
+import Text.Megaparsec.Error
+
+data CompileError =
+  ImportError ImportError
+  | ParseFail (ParseErrorBundle Text Void)
+  
+data ImportError =
+  AmbiguousModule String [FilePath]
+  | ModuleNotFound String
+  | ModuleDeclNotFound String FilePath
+  | CircularImport String FilePath FilePath
+  deriving (Show, Read, Eq, Ord)
